364a3e388fc3de80c5beed0cbc5212e71eda7044
vdhingra-zeda <vaibhavdhingra10@gmail.com>
2020-11-01 00:26:34 +0530

    bug fixed for feedback

--- frontend/src/components/Admin/FeedbackAdmin.jsx
+++ frontend/src/components/Admin/FeedbackAdmin.jsx
@@ -1,55 +1,22 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import DataTable from "react-data-table-component";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import "./FeedbackAdmin.css";
 
 const FeedbackAdmin = () => {
-	const feedbackData1 = [
-		{
-			name: "A",
-			email: "asd@s.com",
-			checkBox: ["presentation", "content"],
-			message: "this is message",
-			rating: "5",
-			date: "1",
-		},
-		{
-			name: "B",
-			email: "mfrcd@4hj.com",
-			checkBox: ["Linguistics", "content"],
-			message: "this is message",
-			rating: "3",
-			date: "5",
-		},
-		{
-			name: "C",
-			email: "r3sd@jg.com",
-			checkBox: ["presentation", "Accuracy", "content"],
-			message:
-				"this is message Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa quibusdam dolores similique cumque, corporis accusantium at recusandae officiis placeat eius, deleniti ipsa neque inventore enim optio libero, ipsum deserunt corrupti.",
-			rating: "1",
-			date: "4",
-		},
-		{
-			name: "D",
-			email: "vrtyu@ss.com",
-			checkBox: ["none"],
-			message: "this is message ",
-			rating: "4",
-			date: "3",
-		},
-	];
-
-	// const feedbackData = [];
-	const [feedbackData, setFeedbackData] = useState([]);
 	
-	fetch("http://localhost:5000/api/get-feedbacks", {
-		method: 'GET'
-	}).then(res => {
-		return res.json();
-	}).then((data) => {
-		setFeedbackData(data);
-	})
+	const [feedbackData, setFeedbackData] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:5000/api/get-feedbacks", {
+			method: 'GET'
+		}).then(res => {
+			return res.json();
+		}).then((data) => {
+			console.log(data);
+			setFeedbackData(data);
+		})
+	}, []);
 
 	const columns = [
 		{
