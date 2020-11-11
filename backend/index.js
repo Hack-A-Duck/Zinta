c9d7cf860ea4fac7fb75e17b3a79e34a717f8d8b
Khush <khushdeep02@gmail.com>
2020-11-12 00:12:11 +0530

    updated README.md

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 connectDB();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(cors());
